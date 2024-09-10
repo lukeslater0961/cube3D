dd819b228ed019132129ff67c48fd8a0d9758ce6
Luke Slater <luke.a.slater@outlook.com>
2024-09-10 11:41:27 +0200

    trying to make a proepr extension checker function , having a hard time getting back into c programming lmao

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -6,26 +6,52 @@
 /*   By: lslater <lslater@42angouleme.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 09:06:55 by lslater           #+#    #+#             */
-/*   Updated: 2024/09/10 09:38:54 by lslater          ###   ########.fr       */
+/*   Updated: 2024/09/10 11:40:25 by lslater          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/parsing.h"
 
-static int	check_extension(char *filename)
+static	int	check_extension(char *filename)
+{
+	char		*extension;
+	int			i;
+	size_t		j;
+
+	extension = ft_strdup(".cub");
+	i = ft_strlen(extension) - 1;
+	ft_printf(1, "%s\n", extension[2]);
+	j = ft_strlen(filename) - 1;
+	while(j >= ft_strlen(extension))
+	{
+		ft_printf(1, "extension[i] = %s, filename[j] = %s\n", extension[i], filename[j]);
+		if (extension[i] != filename[j])
+		{
+			free(extension);
+			return (1);
+		}
+		j--;
+		i--;
+	}
+	free(extension);
+	return (0);
+}
+
+/*static int	check_extension(char *filename)
 {
 	char	*extension;
 
 	extension = ft_strnstr(filename, ".cub", ft_strlen(filename));
 	ft_printf(1, "%s extension\n", extension);
-	extension[ft_strlen(extension) + 1] = '\0';
-	if(!extension || ft_strncmp(extension, ".cub", 4) != 0)
+	if (extension)
+		extension[ft_strlen(extension) + 1] = '\0';
+	if(!extension || strcmp(extension, ".cub") != 0)
 	{
-		ft_printf(2, "filename or extension is invalid\n");
+		ft_printf(2, "Error\n Filename or extension is invalid\n");
 		return (1);
 	}
 	return (0);
-}
+}*/
 
 static	int	 check_directory(char *filename)
 {
@@ -34,12 +60,12 @@ static	int	 check_directory(char *filename)
 	fd = open(filename, O_RDWR);
 	if (errno == EISDIR)
 	{
-		ft_printf(2, "argument is directory and not a file \n");
+		ft_printf(2, "Error\n Argument is directory and not a file \n");
 		return (1);
 	}
 	else if (fd == -1)
 	{
-		ft_printf(2, "file doesn't exist\n");
+		ft_printf(2, "Error\n File doesn't exist\n");
 		return (1);
 	}
 	ft_printf(1, "file is left open because its valid\n");
